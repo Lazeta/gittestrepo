823cb98e02742678c3b4dd55ff2edb1f4b8dcadd
Zam <jcuccllhfppyxoyddfdlhbh@gmail.com>
2025-03-28 02:08:32 +0300

    feat: added closure practice, scope practice

--- index.js
+++ index.js
@@ -63,44 +63,44 @@
 // document.body.append(newElement);
 
 
-function createHTMLElement(tag = 'button', text = 'Кнопка', id) {
-    const element = document.createElement(tag);
-    if (id) element.id = id;
-    if (text) element.innerText = text;
-    return element;
-}
+// function createHTMLElement(tag = 'button', text = 'Кнопка', id) {
+//     const element = document.createElement(tag);
+//     if (id) element.id = id;
+//     if (text) element.innerText = text;
+//     return element;
+// }
 
-function logArgsToConsole() {
-    // console.log(arguments); // arguments - это псевдомассив, который хранит все аргументы в виде массива, которые мы передали в функцию вызова.
-    if(!arguments.length) {
-        console.log('Нет аргументов');
-        return // возвращает undefined
-    }
-    for(let argument of arguments) {
-        console.log(argument);
-    }
-    console.log('Больше аргументов нет');
-}
+// function logArgsToConsole() {
+//     // console.log(arguments); // arguments - это псевдомассив, который хранит все аргументы в виде массива, которые мы передали в функцию вызова.
+//     if(!arguments.length) {
+//         console.log('Нет аргументов');
+//         return // возвращает undefined
+//     }
+//     for(let argument of arguments) {
+//         console.log(argument);
+//     }
+//     console.log('Больше аргументов нет');
+// }
 
-// logArgsToConsole(1, 2, 3, 4, 7, 90);
-logArgsToConsole(); // Нет аргументов, поэтому возвращает undefined и сообщение о том, 
-// что аргументов нет и наша функция завершит работу с помощью return
+// // logArgsToConsole(1, 2, 3, 4, 7, 90);
+// logArgsToConsole(); // Нет аргументов, поэтому возвращает undefined и сообщение о том, 
+// // что аргументов нет и наша функция завершит работу с помощью return
 
-greeting('Name')
-function greeting(name) {
-    console.log(`Hello ${name}! How are you?`);
-}
+// greeting('Name')
+// function greeting(name) {
+//     console.log(`Hello ${name}! How are you?`);
+// }
 // функция вызывается за счёт всплытия - с Function Declaration мы можем вызывать функцию из любого места в рамках области видимости в которой она была объявлена.
 
 
 // _____________________________________________________________
 // Function Expressions
 
-const sayHello = function () {
-    console.log('Hello');
-}
+// const sayHello = function () {
+//     console.log('Hello');
+// }
 
-sayHello(); // наш вызов функции работает не как функция, а как вызов переменной. Мы не можем вызвать её до создания в отличие от Function Declaration.
+// sayHello(); // наш вызов функции работает не как функция, а как вызов переменной. Мы не можем вызвать её до создания в отличие от Function Declaration.
 // мы не можем получить доступ к переменной до того как она была инициализирована.
 
 // const calculateNumber = function (a, b) {
